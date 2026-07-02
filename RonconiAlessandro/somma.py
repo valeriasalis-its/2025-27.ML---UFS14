2e3d2fc4167238e90c55bcd3a5495a8413b73976
alessandroronconi777 <alessandro.ronconi@itsrizzoli.it>
2026-07-02 14:59:19 +0000

    aggiunta file somma

---
+++ RonconiAlessandro/somma.py
@@ -0,0 +1,8 @@
+def somma(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    numero1 = 10
+    numero2 = 15
+    risultato = somma(numero1, numero2)
+    print(f"La somma di {numero1} e {numero2} è uguale a: {risultato}")
